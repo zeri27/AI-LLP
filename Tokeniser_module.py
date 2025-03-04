33933d4e1907723890cfc97fe77dd21930831c7f
Jordy <jdelcastilho@tudelft.nl>
2025-03-04 17:39:31 +0100

    changes

--- Tokeniser_module.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer
-
-def get_tokenised_sections(texts, tokenizer):
-    tokenised_sections = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
-    return tokenised_sections
-
-def get_tokeniser(model_name):
-    tokeniser = AutoTokenizer.from_pretrained(model_name)
-    return tokeniser
