8f5b5cd0b9e632d8661c63151ef77d9b97fa6683
Zeryab Alam <zeryabalam272@gmail.com>
2025-04-07 13:24:26 +0200

    Allow terminal execution

--- src/agent_run.py
+++ src/agent_run.py
@@ -1,5 +1,7 @@
 import asyncio
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from langchain.tools import Tool
 from langchain.memory import ConversationBufferMemory
 from langchain_core.messages import HumanMessage, AIMessage
